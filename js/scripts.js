5f2646b86c61d4aadc4cbd204eb5da9ec721d594
Byron Blocker <byronblocker@gmail.com>
2017-09-14 14:40:47 -0700

    add quote change for male and age bracket

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,11 @@ $(document).ready(function() {
         var gender = parseInt($('input#gender').val());
 
         var quote = (100 - age) * 3;
+        if (gender === 'male' && age < 26) {
+            quote += 50;
+        }
 
-        
+
         $('#rate').empty().append(quote);
         $('#quote').show();
 
